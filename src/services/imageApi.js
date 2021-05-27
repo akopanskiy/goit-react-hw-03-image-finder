947e29c7862e62370f1329b6f702bb4ea1b60b72
akopanskiy <andriikopanskiy@gmail.com>
2021-05-27 11:05:40 +0300

    update imageApi

--- src/services/imageApi.js
+++ src/services/imageApi.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
-const key = '20407857-5cbc70afd557f45317642044e';
-const url = 'https://pixabay.com/api/';
+const KEY = '20407857-5cbc70afd557f45317642044e';
+const URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = URL;
+axios.defaults.params = {
+  key: KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
 const fetchImages = ({ searchQuery = '', page = '' }) => {
   return axios
-    .get(
-      `${url}?q=${searchQuery}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`,
-    )
+    .get(`?q=${searchQuery}&page=${page}&key=${KEY}&`)
     .then(response => response.data.hits);
 };
 
